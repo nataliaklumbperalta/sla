41377c2f5ac10db806a20f51111cb4f7c2a2ef82
nataliaklumbperalta <natalia.peralta@escola.pr.gov.br>
2025-03-17 10:07:12 -0300

    Add files via upload

--- Natalia/script.js
+++ Natalia/script.js
@@ -6,15 +6,19 @@ body {
     height: 100vh; /* Faz o body ocupar 100% da altura da tela */
     margin: 0; /* Remove a margem padrão */
     text-align: center; /* Centraliza os textos */
-
+}
 
 /* Estilo para a div que contém a imagem */
 .imagem-container {
-    text-align: center; /* Garante que a imagem fique centralizada */
+    display: flex; /* Garante centralização adicional */
+    justify-content: center; /* Centraliza horizontalmente */
+    align-items: center; /* Centraliza verticalmente, se necessário */
+    text-align: center; /* Centraliza os textos internamente */
+    width: 50%; /* Garante que ocupe toda a largura */
 }
 
 /* Estilo para as imagens */
 img {
-    max-width: Defina um tamanho; /* A imagem nunca ultrapassará a largura da tela */
+    max-width: 50%; /* A imagem nunca ultrapassará 90% da largura da tela */
     height: auto; /* Mantém a proporção da imagem */
 }
